3a9f9e0b3751f837f846cf994ae3c4996bca5bfe
Jj <jjdelc@gmail.com>
2011-06-30 16:39:52 -0500

    Packaging

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='Django-modal',
+    version='0.1',
+    author='Jj Del Carpio',
+    author_email='jjdelc@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+)
